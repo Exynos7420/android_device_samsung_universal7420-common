f0787bce26e08da878b7052ac28128f74d7625af
Bruno Martins <bgcngm@gmail.com>
2018-04-27 00:04:21 +0100

    universal7420: List additional native libraries to expose to apps
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device-common.mk
+++ device-common.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
 	frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Audio
 PRODUCT_PACKAGES += \
 	audio.primary.universal7420_32 \
