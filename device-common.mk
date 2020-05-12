3e42531fa0fbe3f94702e2edd1ccb1c0e52800f2
enesuzun2002 <enesuzun200227@gmail.com>
2020-05-12 21:21:59 +0300

    universal7420: build graphics packages

--- device-common.mk
+++ device-common.mk
@@ -77,7 +77,19 @@ PRODUCT_PACKAGES += \
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service    
+    android.hardware.configstore@1.0-service
+    
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    gralloc.exynos5 \
+    hwcomposer.exynos5 \
+    libfimg \
+    libhwc2on1adapter \
+    libion
 
 # Wifi
 PRODUCT_PACKAGES += \
