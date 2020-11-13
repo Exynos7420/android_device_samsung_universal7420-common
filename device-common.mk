1b04403ce88b52dd76ae4f5200f5545b64c7c01f
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-11-13 19:55:25 +0300

    universal7420: build sensors hal

--- device-common.mk
+++ device-common.mk
@@ -160,7 +160,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
+	
+# Sensors
+PRODUCT_PACKAGES += \
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
+	
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/init/android.hardware.sensors@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@1.0-service.rc
+	
 # Stagefright-shim
 PRODUCT_PACKAGES += \
 	libstagefright_shim
