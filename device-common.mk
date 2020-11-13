63ac3a157fa68e62873997c79b9d55ebec9b37cf
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-11-13 19:55:25 +0300

    universal7420: build sensors hal
    Change-Id: Ie22ade79a76b13bb7ebd5179be1b24a1a9232d5e

--- device-common.mk
+++ device-common.mk
@@ -147,6 +147,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
