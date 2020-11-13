23e3f515d4ae60ab6b787703f113ad2ff1f036c7
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-11-13 19:59:35 +0300

    universal7420: build vibrator hal
    Change-Id: I6a024430306713aee6a9dfd2b09ba12f25853fb2

--- device-common.mk
+++ device-common.mk
@@ -152,6 +152,11 @@ PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
 	android.hardware.sensors@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+	android.hardware.vibrator@1.0-impl \
+	android.hardware.vibrator@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
