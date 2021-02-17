fe7d72244283224662ca76d5577b193794c74a58
Mikhail Naganov <mnaganov@google.com>
2021-02-17 20:36:38 +0000

    universal7420: Switch audio HAL to V7
    Updated audio policy manager configuration files to comply
    with the changes in the XML schema.
    
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
    Test: atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: I55d3728c809d27e58d5d496a1c7d637050dde53a

--- device-common.mk
+++ device-common.mk
@@ -68,17 +68,20 @@ PRODUCT_PACKAGES += \
 	audio.usb.default \
 	tinymix \
 	android.hardware.audio.service \
-	android.hardware.audio@2.0-impl:32 \
-	android.hardware.audio.effect@2.0-impl:32
+	android.hardware.audio@7.0-impl:32 \
+	android.hardware.audio.effect@7.0-impl:32 \
+	android.hardware.bluetooth.audio@2.0-impl:32
 
 PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
+	frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 	$(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
 	$(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
 
 # Bluetooth
 PRODUCT_PACKAGES += \
