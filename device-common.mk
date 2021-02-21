c031fbda012ecedae17f2b39dcf116d039d8a342
enesuzun2002 <emurat.uzun@gmail.com>
2021-02-21 16:08:25 +0300

    Revert "universal7420: add skeleton for android.hidl.base@1.0"
    This reverts commit 2b16428d12899b2d61d602cc49df7a980707a5f5.
    
    Change-Id: I045b119d98a2cbcbe65343e0700fb6031dfce865

--- device-common.mk
+++ device-common.mk
@@ -212,10 +212,6 @@ PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
 	android.hardware.sensors@1.0-service
 
-# Skeletons
-PRODUCT_PACKAGES += \
-	android.hidl.base@1.0
-
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
