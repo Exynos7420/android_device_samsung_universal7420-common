7b9cbcd042854dbc94b6b855df2faa4155263626
enesuzun2002 <emurat.uzun@gmail.com>
2021-02-22 12:48:14 +0300

    universal7420: passthrough sensors hal
    * Build samsung sensors impl from hardware/samsung
    
    Change-Id: I895cd67f7ca8b6c70eec114d258a9eafabe1f471

--- device-common.mk
+++ device-common.mk
@@ -214,8 +214,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-	android.hardware.sensors@1.0-impl \
-	android.hardware.sensors@1.0-service
+	android.hardware.sensors@1.0-impl.samsung
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
