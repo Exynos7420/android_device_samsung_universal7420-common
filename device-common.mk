e4456d4b8c9ff3498b2efc960e2b4d1785adefac
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    universal7420: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device-common.mk
+++ device-common.mk
@@ -262,6 +262,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/samsung/aidl/power-libperfmgr
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+	
 # Stagefright-shims
 PRODUCT_PACKAGES += \
 	libstagefright_shim
