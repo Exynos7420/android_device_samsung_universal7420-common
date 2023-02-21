4f3aa68208a621f662acb9d3f8d4830d469f057e
enesuzun2002 <emurat.uzun@gmail.com>
2023-02-22 01:51:38 +0300

    universal7420: binderize gnss

--- device-common.mk
+++ device-common.mk
@@ -118,7 +118,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-	android.hardware.gnss@1.0-impl.universal7420
+	android.hardware.gnss@1.0-impl.universal7420 \
+	android.hardware.gnss@1.0-service
 
 # Graphics
 PRODUCT_PACKAGES += \
